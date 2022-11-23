44f8a28824d411787ab75d6ba9bd6d9bb8fc3b5c
romanstetsyk <25715951+romanstetsyk@users.noreply.github.com>
2022-11-23 01:25:36 +0000

    fix tests

--- test/chooseRandomDirection.test.js
+++ test/chooseRandomDirection.test.js
@@ -1,21 +1,16 @@
 import { expect } from "chai";
-import { Battleship } from "../main.js";
-
-const board = new Battleship();
+import { chooseRandomDirection } from "../helpers.js";
 
 describe("chooseRandomDirection", () => {
   it("should return a string", () => {
     for (let i = 0; i < 100; i += 1) {
-      expect(board.chooseRandomDirection()).to.be.a("string");
+      expect(chooseRandomDirection()).to.be.a("string");
     }
   });
 
   it("should return either 'horizontal' or 'vertical'", () => {
     for (let i = 0; i < 100; i += 1) {
-      expect(board.chooseRandomDirection()).to.be.oneOf([
-        "horizontal",
-        "vertical",
-      ]);
+      expect(chooseRandomDirection()).to.be.oneOf(["horizontal", "vertical"]);
     }
   });
 });
--- test/randomInteger.test.js
+++ test/randomInteger.test.js
@@ -1,18 +1,17 @@
 import { expect } from "chai";
 import { Battleship } from "../main.js";
-
-const board = new Battleship();
+import { randomInteger } from "../helpers.js";
 
 describe("randomInteger", () => {
   it("should return an integer", () => {
     for (let i = 0; i < 1000; i += 1) {
-      expect(board.randomInteger(i)).to.satisfy(Number.isInteger);
+      expect(randomInteger(i)).to.satisfy(Number.isInteger);
     }
   });
 
   it("should return a number between 0 and i inclusive, when the input is i", () => {
     for (let i = 0; i < 100; i += 1) {
-      expect(board.randomInteger(i)).to.be.at.least(0).but.at.most(i);
+      expect(randomInteger(i)).to.be.at.least(0).but.at.most(i);
     }
   });
 });
